f4f933f1302110be009907c1abc4d6d9e7cd203f
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-29 08:28:22 -0300

    feat: create function clearFields

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -1,5 +1,5 @@
-class Pet{
-    constructor(tutor,namePet,specie, photo, date){
+class Pet {
+    constructor(tutor, namePet, specie, photo, date) {
         this.tutor = tutor;
         this.namePet = namePet;
         this.specie = specie;
@@ -7,30 +7,31 @@ class Pet{
         this.date = date;
     }
 }
-class PetList{
-    constructor(){
+class PetList {
+    constructor() {
         this.petLista = [];
     }
-    addPet(pet){
+    addPet(pet) {
         this.petLista.push(pet);
         showPet();
+        cleaerFields();
     }
 }
 const petList = new PetList();
-function createPet(){
+function createPet() {
     let tutor = document.getElementById("tutor").value;
     let namePet = document.getElementById("name-pet").value;
     let specie = document.getElementById("species").value;
     let photo = document.getElementById("photo").value;
     let date = document.getElementById("date").value;
-    const pet = new Pet(tutor,namePet,specie,photo,date);
+    const pet = new Pet(tutor, namePet, specie, photo, date);
     petList.addPet(pet);
     console.log(petList);
 }
-function showPet(){
+function showPet() {
     const cotent = document.getElementById("pet-container");
     cotent.innerHTML = "";
-    petList.petLista.forEach(pet =>{
+    petList.petLista.forEach(pet => {
         const petDiv = `
         <h2>Nome do Tutor:${pet.tutor}</h2>
         <p>Nome do Pet:${pet.namePet}</p>
@@ -39,5 +40,12 @@ function showPet(){
         <p>Data:${pet.date}</p>
         `;
         cotent.innerHTML += petDiv;
-});
-}
+    });
+};
+function cleaerFields() {
+    document.getElementById("tutor").value = "";
+    document.getElementById("name-pet").value = "";
+    document.getElementById("species").value = "";
+    document.getElementById("photo").value = "";
+    document.getElementById("date").value = "";
+};
